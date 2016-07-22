3b0460fe1a418a9af81dafe3b25922f2b9b5c3ad
dragonUbuntu <dragonfive2013@163.com>
2016-07-22 11:26:27 +0800

    add cpp11_7_nullptr.cpp

---
+++ cpp11_7_nullptr.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: cpp11_7_nullptr.cpp
+	> Author: DragonFive
+	> Mail: dragonfive2013@163.com
+	> Created Time: 2016年07月22日 星期五 11时24分40秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char * chPtr = nullptr;
+    return 0;
+}
