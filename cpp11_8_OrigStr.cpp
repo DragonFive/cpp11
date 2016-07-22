9e782dde890207b3661bd9fecfa61cc51e1c3fad
dragonUbuntu <dragonfive2013@163.com>
2016-07-22 12:51:51 +0800

    add and modify cpp11_8_OrigStr.cpp

---
+++ cpp11_8_OrigStr.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: cpp11_8_OrigStr.cpp
+	> Author: DragonFive
+	> Mail: dragonfive2013@163.com
+	> Created Time: 2016年07月22日 星期五 12时40分37秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string str = R"("hehe")";
+    cout<<str<<endl;
+    return 0;
+}
