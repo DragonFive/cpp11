c2518653327544a454091034586f79f6dda01576
DragonFive <1690302963@qq.com>
2016-07-25 10:54:49 +0800

    Rename cpp11_5_constuctor.cpp to cpp11_05_constuctor.cpp

--- cpp11_05_constuctor.cpp
+++ cpp11_05_constuctor.cpp
@@ -2,17 +2,17 @@
   *Copyright(C),2011-2018,dragonfive
   *Author:   	dragonfive
   *Version:  	v1.0
-  *Date:        2016��7��21��20:35:42 
-  *Description: ��֤ 
-  *Others:  //��������˵��
-  *Function List:  //��Ҫ�����б���ÿ����¼Ӧ���������������ܼ�Ҫ˵��
-     1.��������
-     2.��������
-  *History:  //�޸���ʷ��¼�б���ÿ���޸ļ�¼Ӧ�����޸����ڡ��޸��߼��޸����ݼ��
+  *Date:        2016年7月21日20:35:42 
+  *Description: 验证 
+  *Others:  //其他内容说明
+  *Function List:  //主要函数列表，每条记录应包含函数名及功能简要说明
+     1.…………
+     2.…………
+  *History:  //修改历史记录列表，每条修改记录应包含修改日期、修改者及修改内容简介
      1.Date:
        Author:
        Modification:
-     2.��������
+     2.…………
 **********************************************************************************/
 #include <iostream>
 using namespace std;
@@ -21,7 +21,7 @@ class A
 public:
 	A(int a):_a(a)
 	{
-		cout<<"A �Ĺ��캯��\n";
+		cout<<"A 的构造函数\n";
 	}
 	void print(double a)
 	{
@@ -33,7 +33,7 @@ class B : public A
 {
 public:
 	using A::print;
-	using A::A;//��仰ʹ��B����һ��B(int)��Ĭ�Ϲ��캯��;��Ĭ�Ϲ��캯��ֻ��һ��; 
+	using A::A;//这句话使得B有了一个B(int)的默认构造函数;而默认构造函数只有一个; 
 	void print(int a,int b)
 	{
 		cout<<"print in B\n";
@@ -43,7 +43,7 @@ public:
 int main()
 {
 	B b(1);
-	//B c;��仰���벻ͨ������ΪBû��������Ĭ�Ϲ��캯��; 
+	//B c;这句话编译不通过，因为B没有这样的默认构造函数; 
 	b.print(1.1);
 		
 	return 0;
