aea46bf2c1854349a9542d6a3018995cbaa20f9c
dragonUbuntu <dragonfive2013@163.com>
2016-07-31 19:57:37 +0800

    tie函数用在式子右边作为引用

--- cpp11_19_tuple.cpp
+++ cpp11_19_tuple.cpp
@@ -16,13 +16,26 @@ int main()
     string str;
     int intNum;
     double douNum;
-
+    
+    //下面是tie函数用在式子的左边;只是复制
     tie(str,intNum,douNum)=tup;
     cout<<str<<" "<<intNum<<" "<<douNum<<endl;
     intNum = 12;
     get<2>(tup)=2.4;
     cout<<get<2>(tup)<<" "<<douNum<<endl;
 
+    //下面是tie函数用在式子的右边;是引用
+    
+    
+    auto tup2 = std::tie(str,intNum,douNum);//这里tup2实际到类型为: std::tuple<int&,string&,double&>;
+    cout<<get<0>(tup2)<<" "<<get<1>(tup2)<<" "<<get<2>(tup2)<<endl;
+    intNum = 14;
+    get<2>(tup2)=3.4;
+    
+    cout<<get<0>(tup2)<<" "<<get<1>(tup2)<<" "<<get<2>(tup2)<<endl;
+    
+    get<1>(tup2)=16;
+    cout<<intNum<<endl;
 
     return 0;
 }
