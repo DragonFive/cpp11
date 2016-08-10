7a415c0291b97bf62662b6bc62b7694cb3b54f24
dragonUbuntu <dragonfive2013@163.com>
2016-08-10 16:05:29 +0800

    修改auto，测试可否用于函数声明

--- cpp11_01_auto.cpp
+++ cpp11_01_auto.cpp
@@ -11,11 +11,17 @@ auto addNum(int a,int b)->decltype(a+b)
 {
 	return a+b;
 }
+auto testAuto();
+auto testAuto()
+{
+    std::cout<<"hehe"<<std::endl;
+}
 int main()
 {
 	auto a = 2;
 	auto b=2.2;
 	auto func = addNum;
 	cout<<func(a,b)<<endl;
-	return 0;
+	testAuto();
+    return 0;
 }
